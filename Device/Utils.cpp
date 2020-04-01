5289c4240f2cf04b8880e00de220fedc61da0d84
Maarten Struys <mstruys-work@outlook.com>
2020-04-01 09:10:28 +0200

    Add additional comment in source file Utils.cpp

--- Device/Utils.cpp
+++ Device/Utils.cpp
@@ -8,6 +8,11 @@
  * Int64ToString is a helper function that allows easy display / logging of long integers. These long integers
  * are mainly used in this application to find out when timers are expired in the main loop, after which
  * some action needs to be taken.
+ * 
+ * uint64_t uiValue - The 64 bit value to be converted to a ShowString.
+ * char *pszValue - Place holder for the converted string.
+ * int iLen - Length of the string place holder. NOTE: Make sure to check the length of the converted
+ *            value to avoid overwriting memory.
  *******************************************************************************************************/
 void Int64ToString(uint64_t uiValue, char *pszValue, int iLen)
 {
@@ -34,11 +39,19 @@ void Int64ToString(uint64_t uiValue, char *pszValue, int iLen)
     DEBUGMSG_FUNC_OUT("");
 }
 
+/********************************************************************************************************
+ * ShowBool is a helper function that allows easy display / logging of boolean values.
+ * A boolean string representation is returned to the caller.
+ *******************************************************************************************************/
 const char *ShowBool(bool theValue)
 {
     return theValue ? "true" : "false";
 }
 
+/********************************************************************************************************
+ * ShowString is a helper function that allows easy display of strings (including NULL values).
+ * The display string is returned to the caller(either containing the original string or "NULL")
+ *******************************************************************************************************/
 const char *ShowString(char *pszTheValue)
 {
     return pszTheValue != NULL ? pszTheValue : "NULL";
